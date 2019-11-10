f47f70c90b9d40842f65395332a68533e660e033
robert2030ca <robert2030ca@gmail.com>
2019-11-10 16:56:36 -0500

    started layout with bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,30 @@ import Graph from './components/Graph.js';
 import logo from './logo.svg';
 import './App.css';
 import 'react-chat-widget/lib/styles.css';
+import 'bootstrap/dist/css/bootstrap.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>
-            Allan AI is coming soon.
-          </h1>
+        <header class="jumbotron">
+          <div class="container">
+            <div class="row">
+              <div class="col-12">
+                <h1> Allan AI </h1>
+                <p> To Help You With The Day to Day </p>
+              </div>
+            </div>
+          </div>
         </header>
+        <div class="row">
+          <div class="col-12 col-md-6">
+            <p> This is content in a column </p>
+          </div>
+          <div class="col-12 col-md">
+            <p> This is content in another column </p>
+          </div>
+        </div>
         <div>
           <Chatbot />
         </div>
